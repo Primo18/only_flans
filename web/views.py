0e139fd01cd13382bc2648eda19b508ffc0d2f2c
Primo18 <danilobo.elo@gmail.com>
2024-06-08 20:31:07 -0400

    add hito 2

--- web/views.py
+++ web/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "web/index.html")
+
+
+def about(request):
+    return render(request, "web/about.html")
+
+
+def contact(request):
+    return render(request, "web/contact.html")
+
+
+def welcome(request):
+    return render(request, "web/welcome.html")
